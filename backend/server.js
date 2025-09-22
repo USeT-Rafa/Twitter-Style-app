7b9be8e2d84dc3aabcc9686901b7b1f879b0456a
USeT-Rafa <rafaelaspinto2001@example.com>
2025-09-22 21:25:01 +0100

    Update DB

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import authRouthes from './routes/auth.routes.js'; 
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+
+dotenv.config();
 
 const app = express();
+
+console.log(process.env.MONGO_URI);
 
 app.use("/api/auth", authRouthes);
 
 app.listen(8000,()=>{
     console.log("Server is running on port 8000");
+    connectDB(process.env.MONGO_URI);
 });
