f70ab3a5597edbf1291cc760174cae729196ef53
USeT-Rafa <rafaelaspinto2001@example.com>
2025-10-01 10:30:30 +0100

    added LoginPage, HomePage, SignUpPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import {Route, Routes} from 'react-router-dom'
 import HomePage from './Pages/Home/HomePage'
 import SignUpPage from './Pages/auth/Signup/SignUpPage'
 import LoginPage from './Pages/auth/Login/LoginPage'
+import Sidebar from './components/common/SideBar'
 
 function App() {
 
   return (
     <div className="flex max-w-6xl mx-auto">
+      <Sidebar/>
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/signup" element={<SignUpPage/>}/>
