92db5a0f9c70b5c6ffc447c27f9ab23a15875946
USeT-Rafa <rafaelaspinto2001@example.com>
2025-10-03 11:41:18 +0100

    Update in Login and Logout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {Route, Routes} from 'react-router-dom'
+import {Navigate, Route, Routes} from 'react-router-dom'
 import HomePage from './Pages/Home/HomePage'
 import SignUpPage from './Pages/auth/Signup/SignUpPage'
 import NotificationPage from './Pages/Notification/NotificationPage'
@@ -7,18 +7,47 @@ import LoginPage from './Pages/auth/Login/LoginPage'
 import Sidebar from './components/common/SideBar'
 import RightPanel from './components/common/RightPanel'
 import {Toaster} from 'react-hot-toast'
+import { useQuery } from '@tanstack/react-query'
+import LoadingSpinner from './components/common/LoadingSpinner'
 
 function App() {
+  const {data:authUser, isLoading,error,isError} = useQuery({
+    queryKey: ['authUser'],
+    queryFn : async ()=>{
+      try {
+        const res = await fetch("/api/auth/me");
+        const data = await res.json();
+        if(!res.ok || data.error){
+          throw new Error(data.message||"Something went wrong");
+        }
+        console.log("authUser is here:", data);
+        return data;
+
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    retry:false,
+  })
+
+  if(isLoading){
+    return(
+      <div className='h-screen flex justify-center items-center'>
+        <LoadingSpinner size='lg'/>
+
+      </div>
+    )
+  }
 
   return (
     <div className="flex max-w-6xl mx-auto">
       <Sidebar/>
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/signup" element={<SignUpPage/>}/>
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/notifications" element={<NotificationPage/>}/>
-        <Route path="/profile/:username" element={<ProfilePage/>}/>
+        <Route path="/" element={authUser ? <HomePage/>: <Navigate to ="/login"/>}/>
+        <Route path="/signup" element={!authUser ? <SignUpPage/>: <Navigate to ="/"/>}/>
+        <Route path="/login" element={!authUser ? <LoginPage/> :<Navigate to ="/"/>}/>
+        <Route path="/notifications" element={!authUser ?<NotificationPage/>:<Navigate to ="/login"/>}/>
+        <Route path="/profile/:username" element={!authUser ?<ProfilePage/>:<Navigate to ="/login"/>}/>
       </Routes>
       <RightPanel/>
       <Toaster/>
--- frontend/src/Pages/auth/Login/LoginPage.jsx
+++ frontend/src/Pages/auth/Login/LoginPage.jsx
@@ -5,14 +5,15 @@ import XSvg from "../../../components/svgs/X";
 
 import { MdOutlineMail } from "react-icons/md";
 import { MdPassword } from "react-icons/md";
-import { useMutation } from "@tanstack/react-query";
-import toast from "react-hot-toast";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 const LoginPage = () => {
 	const [formData, setFormData] = useState({
 		username: "",
 		password: "",
 	});
+
+	const queryClient= useQueryClient();
 
 	const {mutate:LoginMutation, isPending, isError, error}= useMutation({
 		mutationFn : async({username,password})=>{
@@ -36,7 +37,7 @@ const LoginPage = () => {
 			}
 		},
 		onSuccess: ()=>{
-			toast.success("Login successfull");
+			queryClient.invalidateQueries({queryKey:["authUser"]});
 		},
 	})
 
@@ -84,7 +85,7 @@ const LoginPage = () => {
 						/>
 					</label>
 					<button className='btn rounded-full btn-primary text-white'>
-						{isPending ? "Loading..." : "Login"}
+						{isPending ? "Loading..." : "Login"} 
 					</button>
 					{isError && <p className='text-red-500'>{error.message}</p>}
 				</form>
