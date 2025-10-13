19b6ffbb84b9e7c1123b8b4d542c250eecf1ecad
USeT-Rafa <rafaelaspinto2001@example.com>
2025-10-13 11:58:06 +0100

    upload limite added when we create a post

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ cloudinary.config({
 
 const app = express();
 
-app.use(express.json());//to parse resq.body
+app.use(express.json({limit : "5mb"}));//to parse resq.body
 app.use(express.urlencoded({ extended: true }));//to parse form data
 app.use(cookieParser());//to parse cookies
 
