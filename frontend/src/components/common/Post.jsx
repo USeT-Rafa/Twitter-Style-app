49eedf2e7542f8f8ef2685bd4c84edf19d2fae4e
USeT-Rafa <rafaelaspinto2001@example.com>
2025-10-14 10:58:57 +0100

    added comment

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -76,6 +76,40 @@ const Post = ({ post }) => {
 
 	})
 
+	const {mutate:commentPost, isPending: isCommenting}= useMutation({
+		mutationFn: async () =>{
+			try {
+				const res = await fetch(`/api/posts/comment/${post._id}`,{
+					method : "POST",
+					headers : {
+    				"Content-Type":"application/json",
+  				},
+				 body : JSON.stringify({ text: comment}),
+				});
+
+				const data = await res.json();
+
+				if(!res.ok){
+					throw new Error(data.error || "Something went wrong");
+				}
+
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+		onSuccess:()=>{
+			toast.success("Post comment successfully");
+			setComment("");
+			queryClient.invalidateQueries({queryKey:["posts"]});
+			
+		},
+		onError:(error)=>{
+			toast.error(error.message);
+		}
+
+	})
+
 	const postOwner = post.user;
 
 	const isLiked = post.likes.includes(authUser._id);
@@ -85,7 +119,6 @@ const Post = ({ post }) => {
 
 	const formattedDate = "1h";
 
-	const isCommenting = false;
 
 	const handleDeletePost = () => {
 		deletePost();
@@ -93,6 +126,8 @@ const Post = ({ post }) => {
 
 	const handlePostComment = (e) => {
 		e.preventDefault();
+		if(isCommenting)return;
+		commentPost();
 	};
 
 	const handleLikePost = () => {
